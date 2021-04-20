f84a37067a6569620677c5aef06c3e029f779c9a
Katarina <kilamuri@gmail.com>
2021-04-21 00:45:47 +0300

    Печать произведения

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ int main(){
 	printf("Enter integer A and integer B from keyboard\n");
 	scanf("%d", &a);
 	scanf("%d", &b);
-	printf("The minus of A and B is %d", a-b);
+	printf("The minus of A and B is %d\n", a-b);
+	printf("The sum of A and B is %d\n", a+b);
+	printf("The multiplication of A and B is %d\n", a*b);
 	return 0;
 }
